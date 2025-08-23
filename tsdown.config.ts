6c78e633233c54ddc7647654ce5ca64667d01edc
subframe7536 <1667077010@qq.com>
2025-08-23 19:07:14 +0800

    chore!: esm only

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from 'tsdown'
 export default defineConfig({
   entry: ['./src/index.ts'],
   dts: { oxc: true, resolve: true },
-  format: ['esm', 'cjs'],
   exports: true,
 })
